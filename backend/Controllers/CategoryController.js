e1efe91f7693cd15aff5bf3e9ff4c6970f7e5435
programmer9176 <myeducation9176@gmail.com>
2024-08-29 00:30:05 +0500

    category crud operation

--- backend/Controllers/CategoryController.js
+++ backend/Controllers/CategoryController.js
@@ -37,10 +37,93 @@ export const CreateCategoryController = async (req, res) => {
 
     } catch (error) {
         console.error(error);
-        return res.json({
+        return resp.json({
             success: false,
             message: "Something went wrong",
             error: error.message
         });
     }
 };
+
+
+export const AllCategoryController = async (req, resp) => {
+    try {
+        const categories = await CategoryModel.find({});
+        const totalCategories = categories.length;
+        return resp.json({
+            success: true,
+            totalCategories,
+            message: "All category list",
+            categories
+        })
+    } catch (error) {
+        console.error(error);
+        return resp.json({
+            success: false,
+            message: "Something went wrong",
+            error: error.message
+        });
+    }
+}
+
+export const SingleCategoryController = async (req, resp) => {
+    try {
+        const { slug } = req.params;
+
+        const singleCategory = await CategoryModel.findOne({ slug });
+        return resp.json({
+            success: true,
+            message: "Single category",
+            singleCategory: singleCategory.name
+        });
+    } catch (error) {
+        console.error(error);
+        return resp.json({
+            success: false,
+            message: "Something went wrong",
+            error: error.message
+        });
+    }
+}
+
+export const UpdateCategoryController = async (req, resp) => {
+    try {
+        const { id } = req.params;
+        const { name } = req.body
+
+        const updateCategory = await CategoryModel.findByIdAndUpdate(id, { slug: slugify(name) }, { new: true });
+        return resp.json({
+            success: true,
+            message: "Updated category",
+            updateCategory: updateCategory.name,
+            slug: updateCategory.slug
+        });
+    } catch (error) {
+        console.error(error);
+        return resp.json({
+            success: false,
+            message: "Something went wrong",
+            error: error.message
+        });
+    }
+}
+
+export const DeleteCategoryController = async (req, resp) => {
+    try {
+        const { id } = req.params;
+
+        const deleteCategory = await CategoryModel.findByIdAndDelete(id);
+
+        return resp.json({
+            success: true,
+            message: "Deleted category",
+        });
+    } catch (error) {
+        console.error(error);
+        return resp.json({
+            success: false,
+            message: "Something went wrong",
+            error: error.message
+        });
+    }
+}
--- backend/Routes/CategoryRoute.js
+++ backend/Routes/CategoryRoute.js
@@ -1,13 +1,19 @@
 import express from "express";
-import { CreateCategoryController } from "../Controllers/CategoryController.js";
+import { AllCategoryController, CreateCategoryController, DeleteCategoryController, SingleCategoryController, UpdateCategoryController } from "../Controllers/CategoryController.js";
 import { isAdmin } from "../Controllers/authControllers.js";
 import { requireSignin } from "../Middelwares/AuthMiddleware.js";
 
 const router = express.Router();
 
-// The correct order of middleware should be:
-// 1. requireSignin: to check if the user is authenticated
-// 2. isAdmin: to check if the user is an admin
 router.post("/create-category", requireSignin, isAdmin, CreateCategoryController);
+
+router.get("/all-category", AllCategoryController);
+
+
+router.get("/single-category/:slug", SingleCategoryController);
+
+router.put("/update-category/:id", UpdateCategoryController);
+
+router.delete("/delete-category/:id", DeleteCategoryController);
 
 export default router;
